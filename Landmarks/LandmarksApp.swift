26d8707940e472fa9b252029215ded4c5067f502
Rain-Poon <85403806+Rain-Poon@users.noreply.github.com>
2022-11-22 17:57:17 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 潘浩庭 on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
